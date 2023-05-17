3248742a4f61732bb5d20e3c18a7ae3514ca5fba
Brock Benton <86993296+OkBrock@users.noreply.github.com>
2023-05-17 15:18:42 -0600

    Add files via upload

---
+++ Math/src/Main.java
@@ -0,0 +1,40 @@
+public class Main {
+    public static void main(String[] args) {
+        // The following will show common math codes
+
+        // Max of various names:
+        /* double x = 5.8;
+        double y = -1.2;
+
+        double z = Math.max(x, y);
+
+        System.out.println(z);
+        
+        // Absolute value:
+        double y = -1.2;
+
+        double z = Math.abs(y);
+
+        System.out.println(z);
+        
+
+        // Square root function:
+        double t = 16;
+
+        double z = Math.sqrt(t);
+
+        System.out.println(z);
+        */
+
+        // Ceiling/floor functions:
+        double x = 5.8;
+
+        double z = Math.ceil(x);
+        double w = Math.floor(x);
+
+        System.out.println(z);
+        System.out.println(w);
+
+    }
+}
+    
